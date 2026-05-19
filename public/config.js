99a7fb54ece8b4944d973f12d7c0809712d1a193
Jonathan Harris <jharris_gitemail@yahoo.com>
2026-05-19 18:33:14 -0500

    Adding artist link

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@ window.SITE_CONFIG = {
   spotifyEmbeds: [
     {
       title: "Featured Release",
-      src: "https://open.spotify.com/embed/artist/ARTIST_ID_HERE?utm_source=generator"
+      src: "https://open.spotify.com/embed/artist/5fOmvEI1sSuV0SXDN4XdDH"
     }
   ],
   videos: [
@@ -10,5 +10,6 @@ window.SITE_CONFIG = {
       title: "Music video coming soon",
       src: ""
     }
+  
   ]
 };
