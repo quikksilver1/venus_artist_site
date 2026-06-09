0537fc3305d7a6accf75f0084865c1f57df1089c
Jonathan Harris <jharris_gitemail@yahoo.com>
2026-06-09 11:53:54 -0500

    adding Finally See

--- public/config.js
+++ public/config.js
@@ -10,6 +10,11 @@ window.SITE_CONFIG = {
       title: "San Antonio - Austin | Official Music Video",
       src: "assets/videos/san-antonio-austin-official-music-video.mp4",
       type: "mp4"
+    },
+    {
+      title: "Finally See | Official Music Video",
+      src: "assets/videos/Finally_See_With_Intro.mp4",
+      type: "mp4"
     }
   ]
 };
